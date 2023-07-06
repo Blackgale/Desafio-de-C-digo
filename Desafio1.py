17b5ec32d60cbf83c69c169880bccabfcde9bc6e
Blackgale <pedro.h.andrade@outlook.com>
2023-07-06 18:00:07 -0300

    primeiro desafio pronto

---
+++ Desafio1.py
@@ -0,0 +1,12 @@
+###Para o primeiro desafio, teremos que criar uma função para nos ajudar. 
+
+def inverter_ordem_palavras(frase):
+    palavras = frase.split()
+    palavras_invertidas = palavras[::-1]
+    frase_invertida = ' '.join(palavras_invertidas)
+    return frase_invertida
+
+# Exemplo de uso:
+frase = "Hello World! OpenAI is amazing"
+frase_invertida = inverter_ordem_palavras(frase)
+print(frase_invertida)
